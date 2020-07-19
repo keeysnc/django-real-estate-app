643818277a7541bf094b333e045e484b4d05c4a0
Nathan Keeyss <keeysnc@gmail.com>
2020-07-19 09:37:28 -0400

    addsed search form filtering

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # create a function to return the rendered listing url that was requested
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -26,4 +27,39 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # bedrooms
+    if 'bedrooms' in request.GET:
+        bedroom = request.GET['bedrooms']
+        if bedroom:
+            queryset_list = queryset_list.filter(bedroom__lte=bedroom)
+
+    # price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': request.GET
+    }
+
+    return render(request, 'listings/search.html', context)
